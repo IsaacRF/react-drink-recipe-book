eae8ba1e93fd11d44cdca7fad09df51bf12fde37
Isaac R.F <isaacrf239@gmail.com>
2021-06-16 17:53:17 +0200

    Update SearchBar component and add styles

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import './styles.scss';
 
 export interface SearchBarProps {}
 
 const SearchBar: React.FC<SearchBarProps> = () => (
-    <h1>Search</h1>
+    <form className="col-12">
+        <fieldset className="text-center">
+            <legend>Busca bebidas por Categoría o Ingrediente</legend>
+        </fieldset>
+
+        <div className="row mt-4">
+            <div className="col-md-4">
+                <input
+                    name="search"
+                    className="form-control"
+                    type="text"
+                    placeholder="Buscar por ingrediente" />
+            </div>
+
+            <div className="col-md-4">
+                <select
+                    name="category"
+                    className="form-control">
+
+                    <option value="">-- Seleccionar categoría --</option>
+                </select>
+            </div>
+
+            <div className="col-md-4">
+                <input
+                    type="submit"
+                    className="btn btn-block"
+                    value="Buscar bebidas" />
+            </div>
+        </div>
+    </form>
 );
 
 export default SearchBar;
