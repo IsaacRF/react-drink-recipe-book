3a5b62a46d3ab70e65626546f295a4c847b14b9e
Isaac R.F <isaacrf239@gmail.com>
2021-07-06 13:41:34 +0200

    Re-styled cards to flip + zoom them on click

--- src/components/Recipe/index.tsx
+++ src/components/Recipe/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Recipe } from '../../types/Recipe';
 import './styles.scss';
 
@@ -7,17 +7,26 @@ export interface RecipeCompProps {
 }
 
 const RecipeComp: React.FC<RecipeCompProps> = ({recipe}) => {
+    const [selected, setSelected] = useState(false);
+
+    function toggleSelected() {
+        setSelected(!selected);
+    }
+
     return (
-        <div className="col-md-4 mb-3">
-            <div className="card">
-                <h2 className="card-header">{recipe.strDrink}</h2>
-                <img className="card-img-top" src={recipe.strDrinkThumb} alt={`${recipe.strDrink} image`} />
-                <div className="card-body">
-                    <button
-                        type="button"
-                        className="btn btn-block">
-                            View Recipe
-                    </button>
+        <div className={`card-background ${selected ? 'active' : 'col-md-4 mb-3'}`} onClick={() => toggleSelected()}>
+            <div className={ `card-container ${selected ? 'selected' : ''}` } >
+                <div className="card">
+                    <div className="card-front">
+                        <h2 className="card-header">{recipe.strDrink}</h2>
+                        <img className="card-img-top" src={recipe.strDrinkThumb} alt={`${recipe.strDrink} image`} />
+                        <div className="card-body"></div>
+                    </div>
+                    <div className="card-back">
+                        <h1>{recipe.strDrink}</h1>
+                        <p>Lorem ipsum</p>
+                        <p>Dolor sit amet</p>
+                    </div>
                 </div>
             </div>
         </div>
